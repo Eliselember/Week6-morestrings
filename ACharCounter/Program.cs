01fb3381bc8e9ebd9ae6177fe29b9f4c3f9175c2
Opilane TTHK <opilane@tthk.ee>
2021-10-06 10:02:18 +0300

    week6-morestrings

---
+++ ACharCounter/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace ACharCounter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //programm küsib kasutajalt sisestada eesnime
+            //programm küsib sisestada perekonnanime
+            //programm kuvab, mitu 'a' tähte kasutaja eesnimes ja
+            //perekonnanimes kokku on
+
+            int counter = 0;
+
+            Console.WriteLine("sisesta eesnimi:");
+            string firstName = Console.ReadLine();
+
+            Console.WriteLine("sisesta perekonnanimi:");
+            string lastName = Console.ReadLine();
+
+            string fullName = $"{firstName}{lastName}".ToLower();
+
+            foreach (char character in fullName)
+            {
+                if (character == 'a')
+                {
+                    counter++; //counter = counter + 1
+                }
+            }
+
+            Console.WriteLine($"Sinu nimes on {counter} a-tähte");
+        }
+    }
+}
---
+++ AnotherStringToReverse/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace AnotherStringToReverse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //programm kuvab lauset "Hello World!" tagurpidi
+            string hellow = "Hello, world";
+
+            for (int i = hellow.Length - 1; i >= 0; i--)
+            {
+                Console.Write(hellow[i]);
+            }
+          
+        }
+    }
+}
